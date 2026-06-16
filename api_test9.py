50ff7defa95312636c34aad3c96dea8655118387
JJ-QAE <jowita.ryczywolska@gmail.com>
2026-06-16 22:15:18 +0200

    API rest - wrong answears 404, wrong endpoint, empty response

---
+++ api_test9.py
@@ -0,0 +1,20 @@
+import requests
+
+def test_404():
+    response = requests.get(
+        "https://jsonplaceholder.typicode.com/posts/99999"
+    )
+    assert response.status_code == 404
+
+def test_wrong_endpoint():
+    response = requests.get(
+        "https://jsonplaceholder.typicode.com/posts/deosntexsist"
+    )
+    assert response.status_code == 404
+
+def test_blank_response():
+    response = requests.get(
+        "https://jsonplaceholder.typicode.com/posts/deosntexsist"
+    )
+    assert response.status_code == 404
+    assert response.json() == {}
